9b1f8e23dc18f56a1803851ece4cd51e5b18a20e
Andres Felipe Muñoz <munozmarinandresfelipe@gmail.com>
2018-11-16 14:12:32 -0500

    Ajustes de Lottie para IOS

--- SatlockApp/SatlockApp.iOS/AppDelegate.cs
+++ SatlockApp/SatlockApp.iOS/AppDelegate.cs
@@ -6,6 +6,7 @@ using Foundation;
 using UIKit;
 
 using CarouselView.FormsPlugin.iOS;
+using Lottie.Forms.iOS.Renderers;
 
 namespace SatlockApp.iOS
 {
@@ -26,6 +27,7 @@ namespace SatlockApp.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             CarouselViewRenderer.Init();
+            AnimationViewRenderer.Init();
             UINavigationBar.Appearance.TintColor = UIColor.White;
             UINavigationBar.Appearance.BarTintColor = new UIColor(red: 0.91f, green: 0.35f, blue: 0.21f, alpha: 1.0f);
             UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes()
